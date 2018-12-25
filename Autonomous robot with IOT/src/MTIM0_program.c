14bb30f912831634f87f3be31688fe3d5aef77fc
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2018-12-25 02:29:56 +0200

    Update MTIM0_program.c

--- Autonomous robot with IOT/src/MTIM0_program.c
+++ Autonomous robot with IOT/src/MTIM0_program.c
@@ -17,7 +17,7 @@ void MTIM0_Initialize(void)
 	TCCR0  = 0x0D ;/*Initialize Timer 0 with 1024 prescaler in CTC mode*/
 	TIMSK |= 1 << 1;/*Enable Timer 0 Output Compare Match Interrupt*/
 	TIFR  |= 1 << 1;/*Clear Timer 0 Output Compare Match Interrupt Flag*/
-	OCR0   = 79;/*Set to reach TOP after 10ms*/
+	OCR0   = 79;/*Set to reach TOP after ~ 10ms*/
 	SREG  |= 1<<7;/*Enable global interrupt*/
 }
 
