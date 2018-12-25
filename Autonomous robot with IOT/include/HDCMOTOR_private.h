cde161b5dcbc917e87bdfaaccb25b3d953cfabac
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2018-12-25 03:27:44 +0200

    Update HDCMOTOR_private.h

--- Autonomous robot with IOT/include/HDCMOTOR_private.h
+++ Autonomous robot with IOT/include/HDCMOTOR_private.h
@@ -1,14 +1,14 @@
 /*
  * HDCMOTOR_private.h
  *
- *  Created on: ??þ/??þ/????
+ *  Created on: ??Ã¾/??Ã¾/????
  *      Author: H
  */
 
 #ifndef HDCMOTOR_PRIVATE_H_
 #define HDCMOTOR_PRIVATE_H_
 
-#define HMOTOR_PORT			DIO_PORTA
+#define HMOTOR_PORT		DIO_PORTA
 #define LEFT_MOTOR_EN_PIN	DIO_u8Pin0
 #define LEFT_MOTOR_IN1_PIN	DIO_u8Pin2
 #define LEFT_MOTOR_IN2_PIN	DIO_u8Pin3
