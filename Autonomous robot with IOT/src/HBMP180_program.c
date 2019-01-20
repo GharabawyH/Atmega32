9799c2a9d24a6b61e8fbac908b549704b6f91d7e
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2019-01-20 20:45:33 +0200

    Update HBMP180_program.c

--- Autonomous robot with IOT/src/HBMP180_program.c
+++ Autonomous robot with IOT/src/HBMP180_program.c
@@ -1,10 +1,8 @@
 /*
-HBMP180 lib 0x01
-
-copyright (c) Davide Gironi, 2012
-
-Released under GPLv3.
-Please refer to LICENSE file for licensing information.
+ * HBMP180_program.c
+ *
+ *  Created on: ??‏/??‏/????
+ *      Author: H
  */
 
 #include "LSTD_TYPES.h"
