97643234c4710b3d1a8b7c28d870ce8731d39951
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2019-01-20 18:06:20 +0200

    Update MTIM1_private.h

--- Autonomous robot with IOT/include/MTIM1_private.h
+++ Autonomous robot with IOT/include/MTIM1_private.h
@@ -2,8 +2,8 @@
 #define MTIM1_PRIVATE_H
 
 #define SREG	 (*((volatile u8*)0x5F))
-#define TCCR1A  (*((volatile u8*)0x4F))
-#define TCCR1B  (*((volatile u8*)0x4E))
+#define TCCR1A    (*((volatile u8*)0x4F))
+#define TCCR1B    (*((volatile u8*)0x4E))
 #define TCNT1	 (*((volatile u16*)0x4C))
 #define OCR1A	 (*((volatile u16*)0x4A))
 #define OCR1B	 (*((volatile u16*)0x48))
