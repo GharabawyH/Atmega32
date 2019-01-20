4dfc8e7047e69ef1251ed003abd55bb0159600c0
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2019-01-20 18:02:36 +0200

    Update main.c

--- Autonomous robot with IOT/src/main.c
+++ Autonomous robot with IOT/src/main.c
@@ -7,33 +7,31 @@
 #include "HESP_interface.h"
 #include "RTOS_interface.h"
 #include "HBMP180_interface.h"
+#include "HDHT11_interface.h"
 #include "HDCMOTOR_interface.h"
 
 volatile u32 Pressure;
 volatile f64 Temp;
 volatile f64 Altitude;
-volatile u8 S , S1;/*Those two variables are used to debug the operation of the DHT11*/
-volatile u8 Humidity;
+volatile u8  S , S1;
+volatile u8  Humidity;
 volatile u64 Pulse;
 volatile u64 Distance;
+volatile u8  flag;
+volatile u8* Buffer;
 
 void Task1(void)
 {
 	HMOTOR_Stop(LEFT_MOTOR);
 	HMOTOR_Stop(RIGHT_MOTOR);
-	S  = HDHT11_SetupForDataReceive();
-	S1 = HDHT11_CalculateData();
-	if( (S | S1) == 0)
-	{
-		Humidity = HDHT11_GetHumidity();
-	}
+	HDHT11_SetupForDataReceive();
+	HDHT11_CalculateData();
+	Humidity = HDHT11_GetHumidity();
 	Temp 	 = HBMP180_gettemperature();
 	Pressure = HBMP180_getpressure();
 	Altitude = HBMP180_getaltitude();
 	HESP_ConnectToHost();
 	HESP_SendData(Temp , Pressure , Altitude , Humidity);
-	HMOTOR_Enable(LEFT_MOTOR);
-	HMOTOR_Enable(RIGHT_MOTOR);
 }
 
 void Task2(void)
@@ -43,31 +41,45 @@ void Task2(void)
 
 void HULTRASONIC_Decision(void)
 {
-	Pulse = HULTRASONIC_CalculateDistance();
-	Distance = ((Pulse*214375)/(100000000));
-	if(Distance > 10)
+	flag++;
+	if(flag == 3)
 	{
-		HMOTOR_Start(LEFT_MOTOR , Dir1);
-		HMOTOR_Start(RIGHT_MOTOR , Dir1);
-	}
-	else
-	{
-		HMOTOR_Start(LEFT_MOTOR , Dir2);
+		flag = 0;
+		/*Pulse = HULTRASONIC_CalculateDistance();
+		Buffer = IntegerToAscii(Pulse);
+		MUART_Transmit(Buffer);
+		MUART_Transmit("\r");*/
+		Distance = ((Pulse*214375)/(100000000));
+		/*Buffer = IntegerToAscii(Distance);
+		MUART_Transmit(Buffer);
+		MUART_Transmit("\r");*/
+		if((Distance > 25) || (Distance < 8))
+		{
+			HMOTOR_Start(LEFT_MOTOR , Dir2);
+			HMOTOR_Start(RIGHT_MOTOR , Dir2);
+			HMOTOR_PWM_Output(LEFT_MOTOR , 20000);
+			HMOTOR_PWM_Output(RIGHT_MOTOR , 20000);
+		}
+		else
+		{
+			HMOTOR_Start(LEFT_MOTOR , Dir2);
+			HMOTOR_Start(RIGHT_MOTOR , Dir1);
+			HMOTOR_PWM_Output(LEFT_MOTOR , 20000);
+			HMOTOR_PWM_Output(RIGHT_MOTOR , 20000);
+		}
 	}
 }
 
 void main(void)
 {
 	HDHT11_Initialize();
-	HBMP180_init();
-	HESP_INIT();
+	HBMP180_Initialize();
+	HESP_Initialize();
 	MINT0_SetCallBack(HULTRASONIC_Decision);
 	HULTRASONIC_Initialize();
 	HMOTOR_Initialize();
-	HMOTOR_Enable(LEFT_MOTOR);
-	HMOTOR_Enable(RIGHT_MOTOR);
 	RTOS_Initialize();
-	RTOS_CreateTask(0,50,Task2);
+	RTOS_CreateTask(0,5,Task2);
 	RTOS_CreateTask(1,500,Task1);
 	while (1)
 	{
