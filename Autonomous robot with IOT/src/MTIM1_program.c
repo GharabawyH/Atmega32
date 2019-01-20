c13b590f9a0652f39adc7b5ecb4aa8f7e713bf08
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2019-01-20 18:02:01 +0200

    Update MTIM1_program.c

--- Autonomous robot with IOT/src/MTIM1_program.c
+++ Autonomous robot with IOT/src/MTIM1_program.c
@@ -11,13 +11,30 @@ void (*MTIM1_CallBack)(void);
 void MTIM1_Initialize(void)
 {
 	TCCR1A = 0x00;
-	TCCR1B = 0x01;/*Initialize Timer 1 with no prescaler*/
-/*	OCR1A  = 1565;
-	OCR1B  = 25000;
-	TIMSK |= 0x10;
-	TIFR  |= 0x10;
-	SREG  |= 1<<7;*/
+	TCCR1B = 0x01;
+//	OCR1A  = 1565;
+//	OCR1B  = 25000;
+//	TIMSK |= 0x10;
+//	TIFR  |= 0x10;
+	SREG  |= 1<<7;
 	TCNT1 = 0;
+}
+
+void MTIM1_PWM_Initialize(void)
+{
+	TCCR1A = 0xA2;
+	TCCR1B = 0x19;
+	ICR    = 0xFFFF;
+}
+
+void MTIM1_PWM_Output_ChannelA(u16 ChannelA)
+{
+	OCR1A = ChannelA;
+}
+
+void MTIM1_PWM_Output_ChannelB(u16 ChannelB)
+{
+	OCR1B = ChannelB;
 }
 
 ISR(TIMER1_COMPA_vect)
