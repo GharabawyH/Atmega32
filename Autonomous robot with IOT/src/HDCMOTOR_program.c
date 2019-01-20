59a422922b5e6b98abd117c2583cefa1a37485c6
GharabawyH <35193316+GharabawyH@users.noreply.github.com>
2019-01-20 18:00:39 +0200

    Update HDCMOTOR_program.c

--- Autonomous robot with IOT/src/HDCMOTOR_program.c
+++ Autonomous robot with IOT/src/HDCMOTOR_program.c
@@ -1,7 +1,7 @@
 /*
  * HDCMOTOR_program.c
  *
- *  Created on: ??˛/??˛/????
+ *  Created on: ??‚Äè/??‚Äè/????
  *      Author: H
  */
 
@@ -9,21 +9,36 @@
 #include "HDCMOTOR_interface.h"
 #include "HDCMOTOR_private.h"
 #include "MDIO_interface.h"
+#include "MTIM1_interface.h"
 
 
 void HMOTOR_Initialize(void)
 {
-	MDIO_SetPortDirection(HMOTOR_PORT , DIO_PORT_HIGH);
+	MDIO_SetPinDirection(DIO_DDRD,DIO_u8Pin4,DIO_HIGH);
+	MDIO_SetPinDirection(DIO_DDRD,DIO_u8Pin5,DIO_HIGH);
+	MTIM1_PWM_Initialize();
+	MDIO_SetPortDirection(HMOTOR_IN_PORT , DIO_PORT_HIGH);
 }
 
-void HMOTOR_Enable(u8 Motor)
+void HMOTOR_NonPWM_Enable(u8 Motor)
 {
 	switch(Motor)
 	{
-	case LEFT_MOTOR:MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_EN_PIN , DIO_HIGH);break;
-	case RIGHT_MOTOR:MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_EN_PIN , DIO_HIGH);break;
+	case LEFT_MOTOR:MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_EN_PIN , DIO_HIGH);break;
+	case RIGHT_MOTOR:MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_EN_PIN , DIO_HIGH);break;
 	}
 }
+
+void HMOTOR_PWM_Output(u8 Motor , u16 Speed)
+{
+	switch(Motor)
+	{
+	case LEFT_MOTOR:MTIM1_PWM_Output_ChannelB(Speed);break;
+	case RIGHT_MOTOR:MTIM1_PWM_Output_ChannelA(Speed);break;
+	}
+}
+
+
 
 void HMOTOR_Start(u8 Motor , u8 Direction)
 {
@@ -33,36 +48,38 @@ void HMOTOR_Start(u8 Motor , u8 Direction)
 	{
 		switch(Direction)
 		{
-		case Dir1:MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_IN1_PIN , DIO_HIGH);
-				  MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_IN2_PIN , DIO_LOW);
-				  break;
-		case Dir2:MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_IN1_PIN , DIO_LOW);
-				  MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_IN2_PIN , DIO_HIGH);
-				  break;
+		case Dir1:MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN1_PIN , DIO_HIGH);
+		MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN2_PIN , DIO_LOW);
+		break;
+		case Dir2:MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN1_PIN , DIO_LOW);
+		MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN2_PIN , DIO_HIGH);
+		break;
 		}
 		break;
 	}
 	case RIGHT_MOTOR:
+	{
+		switch(Direction)
 		{
-			switch(Direction)
-			{
-			case Dir1:MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_IN3_PIN , DIO_HIGH);
-					  MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_IN4_PIN , DIO_LOW);
-					  break;
-			case Dir2:MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_IN3_PIN , DIO_LOW);
-					  MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_IN4_PIN , DIO_HIGH);
-					  break;
-			}
-			break;
+		case Dir1:MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN3_PIN , DIO_HIGH);
+		MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN4_PIN , DIO_LOW);
+		break;
+		case Dir2:MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN3_PIN , DIO_LOW);
+		MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN4_PIN , DIO_HIGH);
+		break;
 		}
+		break;
+	}
 	}
 }
 
 void HMOTOR_Stop(u8 Motor)
 {
 	switch(Motor)
-		{
-		case LEFT_MOTOR:MDIO_WritePinValue(HMOTOR_PORT , LEFT_MOTOR_EN_PIN , DIO_LOW);break;
-		case RIGHT_MOTOR:MDIO_WritePinValue(HMOTOR_PORT , RIGHT_MOTOR_EN_PIN , DIO_LOW);break;
-		}
+	{
+	case LEFT_MOTOR:MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN1_PIN , DIO_LOW);
+	MDIO_WritePinValue(HMOTOR_IN_PORT , LEFT_MOTOR_IN2_PIN , DIO_LOW);break;
+	case RIGHT_MOTOR:MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN3_PIN , DIO_LOW);
+	MDIO_WritePinValue(HMOTOR_IN_PORT , RIGHT_MOTOR_IN4_PIN , DIO_LOW);break;
+	}
 }
